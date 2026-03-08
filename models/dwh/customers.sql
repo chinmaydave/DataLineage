9649e8fa45f291005c2cf993664ee77abc4238e8
chinmaydavecs@gmail.com <chinmaydavecs@gmail.com>
2026-03-08 02:47:02 +0000

    finalcommit

--- models/dwh/customers.sql
+++ models/dwh/customers.sql
@@ -1,2 +1,17 @@
-{{ config(materialized='table', schema='OMMART_DWH') }}
-select * from {{ ref('stg_customers') }}
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='"customer_id"',
+    schema='OMMART_DWH',
+    on_schema_change='sync_all_columns'
+) }}
+
+select *
+from {{ ref('stg_customers') }}
+
+{% if is_incremental() %}
+where "_fivetran_synced" > (
+    select coalesce(max("_fivetran_synced"), '1900-01-01'::timestamp_tz)
+    from {{ this }}
+)
+{% endif %}
